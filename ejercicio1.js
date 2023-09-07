caeccdb39d5f5ad2561d3f80d0d6c1f46f6c3acd
naza elizeche <nazarenaelizeche@gmail.com>
2023-09-07 09:47:09 -0400

    cambios

--- ejercicio1.js
+++ ejercicio1.js
@@ -3,7 +3,7 @@ console.log("hola");
 for (let i=0; i<10; i++){
     let aleatorio= (Math.floor(Math.random()*(37*5)))+(37*5);
     let numero= aleatorio%37;
-
+    
     console.log("\n");
     console.log(" Nro. de juego: "+ i + "\n Valor aleatorio generado: "+ aleatorio + "\n Nro en que cayo la ruleta: "+ numero);
 }
